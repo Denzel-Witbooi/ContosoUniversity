665ad9f60ac12409935712fa39076b8eee84eda7
Denzel-Witbooi <dwitbooi28@gmail.com>
2022-08-29 23:42:14 +0200

    Modify Course entity

--- ContosoUniversity/Models/Course.cs
+++ ContosoUniversity/Models/Course.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoUniversity.Models
@@ -10,9 +11,41 @@ namespace ContosoUniversity.Models
         /// the course rather than having the database generate it.
         /// </summary>
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Display(Name = "Number")]
         public int CourseID { get; set; }
+
+        [StringLength(50, MinimumLength = 3)]
         public string Title { get; set; }
+
+        [Range(0, 5)]
         public int Credits { get; set; }
+        #region Modify Course entity
+        /*
+            The course entity has a foreign key property DepartmentID 
+            which points to the related Department entity and it 
+            has a Department navigation property.
+         */
+        #endregion
+        #region Foreign key and navigation properties
+        /*
+           -- A course is assigned to one department, 
+                so there's a DepartmentID foreign key and a 
+                 Department navigation property
+           -- A course can have any number of students enrolled in it, 
+                so the Enrollments navigation property is a collection
+         */
+        #endregion
+        public int DepartmentID { get; set; }
+
+        public Department Department { get; set; }
+
+
         public ICollection<Enrollment> Enrollments { get; set; }
+
+        /*
+         * A course may be taught by multiple instructors, 
+         * so the CourseAssignments navigation property is a collection
+         */
+        public ICollection<CourseAssignment> CourseAssignments { get; set; }
     }
 }
---
+++ ContosoUniversity/Models/OfficeAssignment.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    public class OfficeAssignment
+    {
+        #region The Key attribute
+        /* URL: https://docs.microsoft.com/en-us/aspnet/core/data/ef-mvc/complex-data-model?view=aspnetcore-6.0
+            - There's a one-to-zero-or-one relationship between the 
+              Instructor and the OfficeAssignment entities.
+            - An office assignment only exists in relation to the instructor 
+              it's assigned to, and therefore its primary key is also 
+              its foreign key to the Instructor entity.
+            - But the Entity Framework can't automatically recognize 
+              InstructorID as the primary key of this entity because 
+              its name doesn't follow the ID or classnameID naming convention.
+            - Therefore, the Key attribute is used to identify it as the key
+         */
+        #endregion
+        [Key]
+        public int InstructorID { get; set; }
+        [StringLength(50)]
+        [Display(Name = "Office Location")]
+        public string Location { get; set; }
+
+        #region The Instructor navigation property
+        /*
+            -- The Instructor entity has a nullable OfficeAssignment navigation property 
+                (because an instructor might not have an office assignment), 
+            -- and the OfficeAssignment entity has a non-nullable 
+                Instructor navigation property 
+                (because an office assignment can't exist without an instructor 
+            -- InstructorID is non-nullable). 
+                When an Instructor entity has a related OfficeAssignment entity, 
+                each entity will have a reference to the other one in its navigation property. 
+         */
+        #endregion
+        public Instructor Instructor { get; set; }
+    }
+}
